d905f65090d2e4bf86285726966b50163ffe9e99
ZitaRR <Haeggqvist1998@hotmail.com>
2020-12-28 11:18:29 +0100

    Added insertion operator overload for Token Type enum

--- BMC/Token.cpp
+++ BMC/Token.cpp
@@ -3,5 +3,12 @@
 Token::Token(Type type, const std::string value) : type{ type }, value{value} {}
 
 std::ostream& operator<<(std::ostream& stream, const Token& token) {
-	return stream << token.value;
+	return stream << "[" << token.type <<  ", " << token.value << "]";
+}
+
+std::ostream& operator<<(std::ostream& stream, const Token::Type& type) {
+	std::string types[] = {
+		"Number", "Operator"
+	};
+	return stream << types[static_cast<int>(type)];
 }
--- BMC/Token.h
+++ BMC/Token.h
@@ -10,6 +10,7 @@ public:
 	};
 	Token(Type _type, const std::string value);
 	friend std::ostream& operator<<(std::ostream& stream, const Token& token);
+	friend std::ostream& operator<<(std::ostream& stream, const Type& type);
 private:
 	std::string value;
 	Type type;
